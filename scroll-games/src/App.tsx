37f988cbcccdacddc9e067457d109ef22a9210f2
meharaj <meharaj137@gmail.com>
2023-10-20 17:04:36 +0530

    added vite react

---
+++ scroll-games/src/App.tsx
@@ -0,0 +1,16 @@
+import { useMediaQuery } from 'react-responsive'
+import GameContainer from './components/GameContainer/GameContainer'
+const Example = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+  return <div className='app'>
+    <GameContainer/>
+  </div>
+}
+export default Example;
---
+++ scroll-games/src/types.ts
@@ -0,0 +1,9 @@
+export interface Game {
+  buildJsonPath: string;
+  unityLoaderJsPath: string;
+  dataUrl: string;
+  frameworkUrl: string;
+  codeUrl: string;
+
+  // Add other properties as needed
+}
