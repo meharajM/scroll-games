dfa8908facdf45fe952653a0dafe5b2eed3ee83b
meharaj <meharaj137@gmail.com>
2023-11-20 18:30:34 +0530

    added s3 bucket support

--- api/index.js
+++ api/index.js
@@ -1,24 +1,69 @@
 import express from 'express';
 import cors from 'cors';
-import fetch from 'node-fetch';
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { Readable } from 'stream';
 import {fileURLToPath} from 'url';
 import path, {dirname} from 'path';
+import dotenv from 'dotenv';
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 
+dotenv.config();
 // Enable CORS for all routes
 app.use(cors());
-
-// Serve gamesMeta.json file
-app.get('/api/gamesMeta', (req, res) => {
-    console.log("inside gamesMeta ")
-    res.sendFile(path.join(__dirname, './Games/gamesMeta.json'));
+console.log("process.env.AWS_ACCESS_KEY_ID,", process.env.AWS_ACCESS_KEY_ID)
+console.log("process.env.AWS_SECRET_ACCESS_KEY", process.env.AWS_SECRET_ACCESS_KEY)
+// Configure AWS SDK
+const s3 = new S3Client({
+  region: 'ap-south-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
-app.use('/api/Games', (req, res) => {
-    console.log(req.url, "inside Games")
-    const gameDirectory = path.join(__dirname, '/Games', req.url);
-    res.sendFile(gameDirectory);
+
+app.get('/api/gamesMeta', async (req, res) => {
+  console.log("inside gamesMeta ")
+  
+  const params = {
+    Bucket: 'scroll-gmaes',
+    Key: 'games-builds/Games/gamesMeta.json'
+  };
+
+  try {
+    const data = await s3.send(new GetObjectCommand(params));
+    data.Body.pipe(res)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.use('/api/Games', async (req, res) => {
+  console.log(req.url, "inside Games")
+  
+  const params = {
+    Bucket: 'scroll-gmaes',
+    Key: 'games-builds/Games' + req.url
+  };
+
+  // Set the correct MIME type for Unity WebGL files
+  if (req.url.endsWith('.data')) {
+    res.type('application/octet-stream');
+  } else if (req.url.endsWith('.mem')) {
+    res.type('application/octet-stream');
+  } else if (req.url.endsWith('.unityweb')) {
+    res.type('application/octet-stream');
+  }
+
+  try {
+    const data = await s3.send(new GetObjectCommand(params));
+    data.Body.pipe(res);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Check if the code is running on Vercel
