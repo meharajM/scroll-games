4e7a48ca9081265dd1f39e50d5dd17ca815ec1d2
meharaj <meharaj137@gmail.com>
2023-11-22 23:03:47 +0530

    changed keys

--- client/src/components/UserAuth/UserAuth.tsx
+++ client/src/components/UserAuth/UserAuth.tsx
@@ -6,14 +6,15 @@ import 'firebase/compat/analytics';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApHAz8Wxim9sVFO08lf2NZ4z5J7SJ662M",
-    authDomain: "scroll-games.firebaseapp.com",
-    projectId: "scroll-games",
-    storageBucket: "scroll-games.appspot.com",
-    messagingSenderId: "191247528426",
-    appId: "1:191247528426:web:58c94e73efd7ccf84cc293",
-    measurementId: "G-E2D1F83NYV"
-  };
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
+};
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
